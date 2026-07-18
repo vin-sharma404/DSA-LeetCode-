d717437c825243cfa5336fa3ca7f398c35260833
vin-sharma404 <vinayak2004sharma@gmail.com>
2026-07-18 23:11:00 +0530

    Time: 15 ms (15.44%), Space: 62.6 MB (20.29%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -10,8 +10,8 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        if(head==null){
-            return null;
+        if(head==null || head.next==null){
+            return head;
         }
         ArrayList<Integer> list=new ArrayList<>();
         ListNode temp=head;
