8f4c7c3325acd129129e017bd07840ec6572e747
vin-sharma404 <vinayak2004sharma@gmail.com>
2026-07-19 15:37:28 +0530

    Time: 9 ms (99.04%), Space: 59.3 MB (61.02%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -8,25 +8,65 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
 class Solution {
+
     public ListNode sortList(ListNode head) {
-        if(head==null || head.next==null){
+
+        if (head == null || head.next == null)
             return head;
-        }
-        ArrayList<Integer> list=new ArrayList<>();
-        ListNode temp=head;
-        while(temp!=null){
-            list.add(temp.val);
-            temp=temp.next;
-        }
-        Collections.sort(list);
-        temp=head;
 
-        for(int i=0;i<list.size();i++){
-            temp.val=list.get(i);
-            temp=temp.next;
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
 
-        return head;
+        ListNode second=slow.next;
+        slow.next = null;
+
+        ListNode left = sortList(head);
+        ListNode right = sortList(second);
+
+        return merge(left, right);
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2) {
+
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while (l1 != null && l2 != null) {
+
+            if (l1.val <= l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+
+            curr = curr.next;
+        }
+
+        if (l1 != null)
+            curr.next = l1;
+
+        if (l2 != null)
+            curr.next = l2;
+
+        return dummy.next;
     }
 }
