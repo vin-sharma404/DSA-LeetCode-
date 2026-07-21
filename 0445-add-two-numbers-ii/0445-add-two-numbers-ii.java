6762a94bc18883e4f3c44fabcb00571a194e3490
vin-sharma404 <vinayak2004sharma@gmail.com>
2026-07-21 22:43:48 +0530

    Time: 1 ms (100%), Space: 46.6 MB (30.1%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
@@ -8,51 +8,54 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        // ListNode dummy=new ListNode(0);
-        ListNode curr1= l1;
-        ListNode curr2=l2;
-        ListNode prev1=null;
-        ListNode prev2=null;
-        while(curr1!=null ){
-            ListNode next1=curr1.next;
-            curr1.next=prev1;
-            prev1=curr1;
-            curr1=next1;
-        }
-        while(curr2!=null){
-            ListNode next2=curr2.next;
-            curr2.next=prev2;
-            prev2=curr2;
-            curr2=next2;
-        }
-        ListNode dummy = new ListNode(0);
-        ListNode temp=dummy;
-        int carry=0;
-        while(prev1!=null || prev2!=null || carry!=0){
-            int sum=carry;
-            if(prev1!=null){
-                sum+=prev1.val;
-                prev1=prev1.next;
-            }
-            if(prev2!=null){
-                sum+=prev2.val;
-                prev2=prev2.next;
-            }
-            carry=sum/10;
-            temp.next=new ListNode(sum%10);
-            temp=temp.next;
+
+ class Solution {
+
+    public ListNode reverse(ListNode head){
+
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while(curr != null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
         }
 
-        ListNode temp2=dummy.next;
-        ListNode prev3=null;
-        while(temp2!=null){
-            ListNode next3=temp2.next;
-            temp2.next=prev3;
-            prev3=temp2;
-            temp2=next3;
+        return prev;
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        l1 = reverse(l1);
+        l2 = reverse(l2);
+
+        ListNode dummy = new ListNode(0);
+        ListNode temp = dummy;
+
+        int carry = 0;
+
+        while(l1 != null || l2 != null || carry != 0){
+
+            int sum = carry;
+
+            if(l1 != null){
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if(l2 != null){
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            carry = sum / 10;
+
+            temp.next = new ListNode(sum % 10);
+            temp = temp.next;
         }
-        return prev3;
+
+        return reverse(dummy.next);
     }
 }
