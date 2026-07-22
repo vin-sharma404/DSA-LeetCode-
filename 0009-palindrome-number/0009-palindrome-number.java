13b1486303e160d8dfcd110970699203d004cc54
vin-sharma404 <vinayak2004sharma@gmail.com>
2026-07-22 22:19:21 +0530

    Time: 5 ms (80.84%), Space: 46.1 MB (16.94%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,15 +1,12 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String s= String.valueOf(x);
-        int left=0;
-        int right=s.length()-1;
-        while(left<right){
-            if(s.charAt(left)!=s.charAt(right)){
-                return false;
-            }
-            left++;
-            right--;
+        int original=x;
+        int rev=0;
+        while(x>0){
+            int rem=x%10;
+            rev=rev*10+rem;
+            x=x/10;
         }
-        return true;
+        return rev==original;
     }
 }
