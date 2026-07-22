2040901a7b80ac5c6ee758f022b42426b42c0918
vin-sharma404 <vinayak2004sharma@gmail.com>
2026-07-22 10:56:04 +0530

    Time: 3 ms (99.87%), Space: 46.2 MB (78.15%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,33 +1,28 @@
 class Solution {
     public String intToRoman(int num) {
-        Map<Integer, String> map = new HashMap<>();
+            int[] value = {
+            1000,900,500,400,
+            100,90,50,40,
+            10,9,5,4,1
+        };
 
-        map.put(1000, "M");
-        map.put(900, "CM");
-        map.put(500, "D");
-        map.put(400, "CD");
-        map.put(100, "C");
-        map.put(90, "XC");
-        map.put(50, "L");
-        map.put(40, "XL");
-        map.put(10, "X");
-        map.put(9, "IX");
-        map.put(5, "V");
-        map.put(4, "IV");
-        map.put(1, "I");
-
-        int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] roman = {
+            "M","CM","D","CD",
+            "C","XC","L","XL",
+            "X","IX","V","IV","I"
+        };
 
         StringBuilder ans = new StringBuilder();
 
-        for (int value : values) {
-            while (num >= value) {
-                ans.append(map.get(value));
-                num -= value;
+        for(int i = 0; i < value.length; i++) {
+
+            while(num >= value[i]) {
+                ans.append(roman[i]);
+                num -= value[i];
             }
         }
 
         return ans.toString();
+
     }
 }
-    
